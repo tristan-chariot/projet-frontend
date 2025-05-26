2d0fce6481cb42b962d283a602901f4781ec05a9
Tristan Chariot <tristan.chariot@etu.minesparis.psl.eu>
2025-05-26 17:29:49 +0200

    séparation html / ccs / js pour clarifier le code

--- pixels-war.js
+++ pixels-war.js
@@ -1,96 +1,95 @@
-// pour l'instant on ne peut pas y toucher depuis l'interface
-// il faut recharger la page pour changer de carte
-const PIXEL_URL = "https://pixels-war.oie-lab.net"
+const adresse = document.querySelector("#adresse").value;
+const carte = document.querySelector("#map").value;
+const couleur = document.querySelector("#couleur").value;
+console.log({ adresse, carte, couleur })
 
-// c'est sans doute habile de commencer avec la carte de test
-// const MAP_ID = "0000"
-const MAP_ID = "TEST"
+const mapElement = document.querySelector("#mapcontainer")
+
+async function preinit() {
+    const res = await fetch(`${adresse}/api/v1/${carte}/preinit`, { credentials: "include" });
+    const { key } = await res.json();
+    init(key)
+}
+
+
+async function init(key) {
+    const res = await fetch(`${adresse}/api/v1/${carte}/init?key=${key}`, { credentials: "include" });
+    const { id, nx, ny, timeout, data } = await res.json()
+
+    let contenu = "";
+    for (let col = 0; col < nx; col++) {
+        for (let li = 0; li < ny; li++) {
+            const [r, g, b] = data[li][col];
+            contenu += `<div class="pixel" id="l${li}_c${col}" style="background-color: rgb(${r}, ${g}, ${b})"></div>`;
+        }
+
+        mapElement.innerHTML = contenu;
+        mapElement.style.setProperty('--colonnes', nx);
+        mapElement.style.setProperty('--lignes', ny);
+
+        console.log(contenu);
+
+        setInterval(() => deltas(id), 1000);
+
+
+        document.querySelectorAll(".pixel").forEach(pixel => {
+            pixel.addEventListener("click", async (click) => {
+                const idPixel = click.target.id;
+                const posPixel = idPixel.match(/l(\d+)_c(\d+)/)
+                const li = posPixel[1]
+                const col = posPixel[2]
+                const [r, g, b] = getPickedColorInRGB()
+                const res = await fetch(`${adresse}/api/v1/${carte}/set/${id}/${li}/${col}/${r}/${g}/${b}`, { credentials: "include" })
+                const resultat = await res.json();
+
+                if (resultat == 0) {
+                    deltas(id);
+                }
+                else {
+                    const tempsattente = Math.round(resultat / 10 ** 9);
+                    alert(`Vous devez patienter ${tempsattente} secondes avant de pouvoir changer la couleur d'un pixel.`);
+                }
+            })
+        })
+    }
+
+
+}
+
+
+async function deltas(id) {
+    const res = await fetch(`${adresse}/api/v1/${carte}/deltas?id=${id}`, { credentials: "include" })
+    const { deltas } = await res.json();
+    for ([x, y, r, g, b] of deltas) {
+        console.log("delta", [x, y, r, g, b]);
+        const pixel = document.querySelector(`#l${x}_c${y}`);
+        pixel.style.backgroundColor = `rgb(${r},${g},${b})`;
+    }
+}
+
+
+function getPickedColorInRGB() {
+    const colorHexa = document.getElementById("couleur").value
+
+    const r = parseInt(colorHexa.substring(1, 3), 16)
+    const g = parseInt(colorHexa.substring(3, 5), 16)
+    const b = parseInt(colorHexa.substring(5, 7), 16)
+
+    return [r, g, b]
+}
+
+
 
 document.addEventListener("DOMContentLoaded", () => {
+    const carte = document.querySelector("#map").value;
+    const description = document.querySelector("#description-carte");
+    description.textContent = `Bienvenue sur la carte ${carte} !`;
+});
 
-    const PREFIX = `${PIXEL_URL}/api/v1/${MAP_ID}`
 
-    // pour savoir à quel serveur / carte on s'adresse
-    // on les affiche en dur
-    // pour l'instant on ne peut pas y toucher depuis l'interface
-    // il faut recharger la page pour changer de carte
-    document.getElementById("baseurl").value = PIXEL_URL
-    document.getElementById("mapid").value = MAP_ID
-    document.getElementById("baseurl").readOnly = true
-    document.getElementById("mapid").readOnly = true
 
-    fetch(`${PREFIX}/preinit`, {credentials: "include"})
-        .then((response) => response.json())
-        .then((json) => {
-            console.log(json)
-            //TODO: maintenant que j'ai le json de preinit, je peux initialiser ma grille
+preinit()
 
-            //TODO: maintenant que j'ai le JSON, afficher la grille, et récupérer l'id
 
-            //TODO: maintenant que j'ai l'id, attacher la fonction refresh(id), à compléter, au clic du bouton refresh
-
-            //TODO: attacher au clic de chaque pixel une fonction qui demande au serveur de colorer le pixel sous là forme :
-            // http://pixels-war.oie-lab.net/api/v1/0000/set/id/x/y/r/g/b
-            // la fonction getPickedColorInRGB ci-dessous peut aider
-
-            //TODO: pourquoi pas rafraichir la grille toutes les 3 sec ?
-            // voire même rafraichir la grille après avoir cliqué sur un pixel ?
-
-            // cosmétique / commodité / bonus:
-
-            // TODO: pour être efficace, il serait utile d'afficher quelque part
-            // les coordonnées du pixel survolé par la souris
-
-            //TODO: pour les rapides: afficher quelque part combien de temps
-            // il faut attendre avant de pouvoir poster à nouveau
-
-            //TODO: pour les avancés: ça pourrait être utile de pouvoir
-            // choisir la couleur à partir d'un pixel ?
-
-        })
-
-    //TODO: pour les élèves avancés, comment transformer les "then" ci-dessus en "async / await" ?
-    //TODO: pour les élèves avancés, faire en sorte qu'on puisse changer de carte
-    //      voir le bouton Connect dans le HTML
-
-    // À compléter puis à attacher au bouton refresh en passant mon id une fois récupéré
-    function refresh(user_id) {
-        fetch(`${PREFIX}/deltas?id=${user_id}`, {credentials: "include"})
-            .then((response) => response.json())
-            .then((json) => {
-                //TODO: maintenant que j'ai le json des deltas, mettre à jour les pixels qui ont changé.
-                // "Here be dragons" : comment récupérer le bon div ?
-
-            })
-    }
-
-    // Petite fonction facilitatrice pour récupérer la couleur cliquée en RGB
-    function getPickedColorInRGB() {
-        const colorHexa = document.getElementById("colorpicker").value
-
-        const r = parseInt(colorHexa.substring(1, 3), 16)
-        const g = parseInt(colorHexa.substring(3, 5), 16)
-        const b = parseInt(colorHexa.substring(5, 7), 16)
-
-        return [r, g, b]
-    }
-
-    // dans l'autre sens, pour mettre la couleur d'un pixel dans le color picker
-    // (le color picker insiste pour avoir une couleur en hexadécimal...)
-    function pickColorFrom(div) {
-        // plutôt que de prendre div.style.backgroundColor
-        // dont on ne connait pas forcément le format
-        // on utilise ceci qui retourne un 'rbg(r, g, b)'
-        const bg = window.getComputedStyle(div).backgroundColor
-        // on garde les 3 nombres dans un tableau de chaines
-        const [r, g, b] = bg.match(/\d+/g)
-        // on les convertit en hexadécimal
-        const rh = parseInt(r).toString(16).padStart(2, '0')
-        const gh = parseInt(g).toString(16).padStart(2, '0')
-        const bh = parseInt(b).toString(16).padStart(2, '0')
-        const hex = `#${rh}${gh}${bh}`
-        // on met la couleur dans le color picker
-        document.getElementById("colorpicker").value = hex
-    }
-
-})
+const description = document.querySelector("#map-description");
+description.textContent = `Bienvenue sur la carte ${carte} !`;
